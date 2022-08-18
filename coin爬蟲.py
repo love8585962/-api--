591628aecd5139d05d323324d97e1fb351849340
Louis Chen <love8585962@gmail.com>
2022-08-18 23:58:08 +0800

    add new file

---
+++ coin爬蟲.py
@@ -0,0 +1,41 @@
+import time
+import requests
+import json
+
+while True:
+    # Defining Binance API URL
+
+    key = "https://api.binance.com/api/v3/ticker/price?symbol="
+
+    # Making list for multiple crypto's
+
+    currencies = ["BTCUSDT", "ETHUSDT", "ADAUSDT"]
+
+    j = 0
+
+    # running loop to print all crypto prices
+
+    for i in currencies:
+
+            # completing API for request
+
+        url = key+currencies[j]
+
+        data = requests.get(url)
+
+        data = data.json()
+
+        j = j+1
+
+
+    # 發送到 line notify
+        headers = {
+            "Authorization": "Bearer " + "Yu530PSGt1K0Kasc4CJrK6wdSVaxnTyZ65r3WZMbq1f",
+            "Content-Type": "application/x-www-form-urlencoded"
+        }
+
+        params = {'message' : f"{data['symbol']} $ {float(data['price'])}"}
+
+        r = requests.post("https://notify-api.line.me/api/notify", headers=headers, params=params)
+        print(params)
+    time.sleep(30)
